3773ae8dd75c413cde3b2a595a3ccf049f77a1e7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-02-06 15:58:52 +0530

    fixes

--- src/firebase.js
+++ src/firebase.js
@@ -4,5 +4,5 @@ const firebaseConfig = {
   projectId: "hn-news-4aba9",
   storageBucket: "hn-news-4aba9.appspot.com",
   messagingSenderId: "853169906432",
-  appId: "1:853169906432:web:eb7bfba3c3959e617a89e5",
+  appId: "1:853169906432:web:8182d2c7ad7219467a89e5",
 };
