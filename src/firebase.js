ec4f1b8e976171979116d3ac5e30024afe759339
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-02-06 15:44:11 +0530

    hosting

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUA4dBGMfFPQbpxJ-1etgbn1yFn_HwwJM",
+  authDomain: "hn-news-4aba9.firebaseapp.com",
+  projectId: "hn-news-4aba9",
+  storageBucket: "hn-news-4aba9.appspot.com",
+  messagingSenderId: "853169906432",
+  appId: "1:853169906432:web:eb7bfba3c3959e617a89e5",
+};
